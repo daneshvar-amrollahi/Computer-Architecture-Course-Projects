f3ecdc9b637cf6a1b608ee924ccd5897c514a258
daneshvar.amrollahi <amrollahi.daneshvar@gmail.com>
2020-12-19 11:33:50 +0330

    fixed datapath

--- MIPS-MultiCycle/datapath.v
+++ MIPS-MultiCycle/datapath.v
@@ -9,10 +9,9 @@ module datapath (clk,
                  pc_src,
                  alu_ctrl,
                  reg_write,
-                 zero,
-                 data_to_write,
-                 jr,
-                 jmp,
+                 zero_out,
+                 zero_in,
+                 adr_to_write,
                  IorD,
                  pc_write,
                  pc_write_cond,
@@ -21,39 +20,32 @@ module datapath (clk,
     input  clk, rst;
     output [31:0] data_to_mem;
     input  [31:0] mem; //could be data, could be an instruction
-    input  [31:0] data_in;
-    input mem_to_reg, alu_src, pc_src, reg_write;
+    input [1:0] reg_dst;
+    input mem_to_reg, alu_src_a, reg_write, ir_write, IorD, pc_write, pc_write_cond, zero_in, zero_out;
+    input [1:0] alu_src_b;
+    intput [1:0] pc_src;
     input  [2:0] alu_ctrl;
-    output zero;
+    output adr_to_write[31:0];
     
-    //our signals coming from controller;
-    input jr, jmp;
-    //slti;
-    input [1:0] data_to_write,reg_dst;
-    
-    wire [31:0] PC_OUT;
+    wire [31:0] pc_out;
     wire [31:0] read_data1, read_data2;
     wire [31:0] sgn_ext_out;
     wire [4:0] mux2_out;
     wire [31:0] alu_out;
-    wire [31:0] adder2_out;
     wire [31:0] shl2_32_out;
     wire [31:0] mux3_out;
     wire [31:0] mux4_out;
+    wire [31:0] mux1_out;
     
-    wire [31:0]  mux1_out;
-    
-    //out nets
     wire [31:0] mux5_out;
     wire [27:0] shl2_26_out;
     wire [31:0] mux6_out;
     wire [31:0] ir_out;
-    wire [31:0] sgn_ext_10_out;
     wire [31:0] mdr_out;
     wire [31:0] a_out;
     wire [31:0] b_out;
 
-    reg_32b PC(mux6_out, rst, 1'b1, clk, Ppc_out);
+    reg_32b PC(mux6_out, rst, (zero_in & pc_write_cond) | pc_write, clk, pc_out);
 
     mux4to1_32b MUX1(pc_out, aluout_out, IorD, mux1_out);    
     
@@ -61,7 +53,7 @@ module datapath (clk,
 
     reg_32b MDR(mem, rst, 1'b1, clk, mdr_out);
 
-    mux2to1_5b MUX2(ir_out[20:16], ir_out[15:11], reg_dst, mux2_out);
+    mux3to1_5b MUX2(ir_out[20:16], ir_out[15:11], 5'b11111, reg_dst, mux2_out);
 
     mux2to1_32b MUX3(aluout_out, mdr_out, mem_to_reg, mux3_out);
 
@@ -77,11 +69,13 @@ module datapath (clk,
 
     mux4to1_32b MUX5(b_out, {28'b0, 4'b0100}, sgn_ext_out, shl2_32_out, alu_src_b, mux5_out);
 
-    alu ALU(mux4_out, mux5_out, alu_ctrl, alu_out, zero);
+    alu ALU(mux4_out, mux5_out, alu_ctrl, alu_out, zero_out);
 
     shl2_26b SHL2_26B(ir_out[25:0], shl2_26_out);
 
     reg_32b ALUOUT(alu_out, rst, 1'b1, clk, aluout_out);
 
-    mux4to1_32b(alu_out, shl2_26_out, aluout_out, a_out, pc_src, mux6_out);
+    mux4to1_32b MUX6(alu_out, {pc_out[31:28], shl2_26_out}, aluout_out, a_out, pc_src, mux6_out);
+
+    assign adr_to_write = mux1_out;
 endmodule
