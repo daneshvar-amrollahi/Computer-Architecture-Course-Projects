46f9ace7b75539616570a30e5a1b8262ff2f7517
daneshvar.amrollahi <amrollahi.daneshvar@gmail.com>
2020-12-27 02:50:36 +0330

    final changes

--- MIPS-MultiCycle/memory.v
+++ MIPS-MultiCycle/memory.v
@@ -1,18 +1,18 @@
 
 module memory(adr,
-                 d_in,
-                 mrd,
-                 mwr,
-                 clk,
-                 d_out,
-                 two_thousand,
-                 two_thousand_four);
+              d_in,
+              mrd,
+              mwr,
+              clk,
+              d_out,
+              two_thousand,
+              two_thousand_four);
     input [31:0] adr;
     input [31:0] d_in;
     input mrd, mwr, clk;
     output [31:0] d_out;
     output [31:0] two_thousand, two_thousand_four;
-    reg [7:0] mem[0:65535]; 
+    reg [7:0] mem[0:65535];
     
     initial
     begin
@@ -26,7 +26,7 @@ module memory(adr,
         if (mwr == 1'b1) begin
             {mem[adr+3], mem[adr+2], mem[adr+1], mem[adr]} = d_in;
         end
-
-    assign two_thousand = {mem[2000+3], mem[2000+2], mem[2000+1], mem[2000]};
+    
+    assign two_thousand      = {mem[2000+3], mem[2000+2], mem[2000+1], mem[2000]};
     assign two_thousand_four = {mem[2004+3], mem[2004+2], mem[2004+1], mem[2004]};
 endmodule
